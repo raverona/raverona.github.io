38a0993ab40e27494e93615dba4e25ceed1ba0cf
Rafael Verona <raverona@gmail.com>
2020-05-03 04:17:27 -0300

    Add icon to open and close menu on small screens and fix animation when accessing from the projects page

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,30 +6,30 @@ sitemap:
 
 $(document).ready(function () {
   $('a.panel-button').click(function (e) {
-    //if ($('.panel-cover').hasClass('panel-cover--collapsed')) return
     if ($('.content-wrapper').hasClass('showing')){
-      $('.content-wrapper').removeClass('animated slideInRight')
-      $('.panel-cover').removeClass('panel-cover--collapsed')
-      $('.panel-cover').css('max-width', '100%')
-      $('.panel-cover').animate({'width': '100%'}, 400, swing = 'swing', function () {})
-      $('.content-wrapper').removeClass('showing')
+      $('.content-wrapper').removeClass('animated slideInRight');
+      $('.panel-cover').removeClass('panel-cover--collapsed');
+      $('.panel-cover').css('max-width', '100%');
+      $('.panel-cover').animate({'width': '100%'}, 400, swing = 'swing', function () {});
+      $('.content-wrapper').removeClass('showing');
       window.location.hash = '';
-      parent.location.hash = ''
+      parent.location.hash = '';
       return;
     }
-    currentWidth = $('.panel-cover').width()
+    currentWidth = $('.panel-cover').width();
     if (currentWidth < 960) {
-      $('.panel-cover').addClass('panel-cover--collapsed')
+      $('.panel-cover').addClass('panel-cover--collapsed');
       $('.content-wrapper').addClass('animated slideInRight')
     } else {
-      $('.panel-cover').css('max-width', currentWidth)
+      $('.panel-cover').css('max-width', currentWidth);
       $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
     }
     $('.content-wrapper').addClass('showing');
-  })
+  });
 
   if (window.location.hash && window.location.hash == '#projects') {
-    $('.panel-cover').addClass('panel-cover--collapsed')
+    $('.content-wrapper').addClass('showing');
+    $('.panel-cover').css({'max-width': '530px', 'width': '40%'})
   }
 
   if (window.location.pathname !== '{{ site.baseurl }}/' && window.location.pathname !== '{{ site.baseurl }}/index.html') {
@@ -37,12 +37,12 @@ $(document).ready(function () {
   }
 
   $('.btn-mobile-menu').click(function () {
-    $('.navigation-wrapper').toggleClass('visible animated bounceInDown')
-    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
-  })
+    $('.navigation-wrapper').toggleClass('visible animated bounceInDown');
+    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn hidden')
+  });
 
   $('.navigation-wrapper .projects-button').click(function () {
-    $('.navigation-wrapper').toggleClass('visible')
+    $('.navigation-wrapper').toggleClass('visible');
     $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
   })
-})
+});
